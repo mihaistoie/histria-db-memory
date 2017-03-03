e8902c50aed9e6de0ffd70a8e68edce17fcf4bd7
Mihai  STOIE <mihaistoie@gmail.com>
2017-03-03 10:31:15 +0100

    project structure changed

---
+++ test/memory-storage.specs.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//import { helper } from '../src/index';
+describe('Memory storage', () => {
+    it('Initialize', function () {
+    });
+});
